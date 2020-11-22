7bd87a08ab0b0a7eceb6a74524f494fb39a10e0b
MaelVB <vaillant.beuchot.mael@gmail.com>
2020-11-22 22:59:10 +0100

    [new] Add the first version of the snowball with stone

---
+++ Items/SnowballWithStone.cs
@@ -0,0 +1,54 @@
+using SnowballFight.Projectiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SnowballFight.Items
+{
+    public class SnowballWithStone : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // DisplayName.SetDefault("TutorialSword"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+            Tooltip.SetDefault("A snowball with a stone");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 15;
+            item.width = 20;
+            item.height = 20;
+
+            item.ranged = true;
+            item.useTime = 20;
+            item.useAnimation = 20;
+            item.useStyle = 1;
+            item.knockBack = 6.0f;
+
+            item.value = 10000;
+            item.rare = 2;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = false;
+
+            item.shoot = ModContent.ProjectileType<SnowballWithStoneProjectile>();
+            item.shootSpeed = 8f;
+            item.consumable = true;
+            item.maxStack = 999;
+
+            item.noMelee = true;
+            item.noUseGraphic = true;
+
+            item.ammo = AmmoID.Snowball;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Snowball, 2);
+            recipe.AddIngredient(ItemID.StoneBlock, 1);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this, 1);
+            recipe.AddRecipe();
+        }
+    }
+}
